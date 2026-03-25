4c94cea4884fcee883651ec9c1a8ef84eee71e1b
Samiran Sarkar <samiransarka123@gmail.com>
2026-03-25 22:22:01 +0530

    // Arguments Can Be Variables
    let x = 10;
    let y = 18;
    
    function add(a, b){
        return a + b;
    }
    
    console.log(add(x, y));

---
+++ JavaScript Function Arguments/Arguments Can Be Variables/1.js
@@ -0,0 +1,11 @@
+// Arguments Can Be Variables
+
+let x = 10;
+let y = 18;
+
+
+function add(a, b){
+    return a + b;
+}
+
+console.log(add(x, y));
---
+++ JavaScript Function Arguments/The Order of Arguments Matters/1.js
@@ -0,0 +1,10 @@
+// The Order of Arguments Matters
+
+
+function subtract(a, b){
+    return a - b;
+}
+x = subtract(10, 5);
+y = subtract(5, 10);
+
+console.log(x, y);
